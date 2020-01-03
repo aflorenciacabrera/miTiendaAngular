0c4f326b19ff218e5011d51af2a514923a22729f
Florencia Cabrera <fcabrera@invico.gov.ar>
2020-01-03 11:53:28 -0300

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductoFormComponent } from './components/productos/producto-form/producto-form.component';
 import { ProductoGaleriaComponent } from './components/productos/producto-galeria/producto-galeria.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,7 +24,9 @@ import { ProductoGaleriaComponent } from './components/productos/producto-galeri
   imports: [
     BrowserModule,
     AppRoutingModule,
-    APP_ROUTING
+    APP_ROUTING,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/productos/producto-form/producto-form.component.ts
+++ src/app/components/productos/producto-form/producto-form.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-producto-form',
   templateUrl: './producto-form.component.html',
   styleUrls: ['./producto-form.component.css']
 })
 export class ProductoFormComponent implements OnInit {
-
+  productos = {
+    titulo: '',
+    tipo: '',
+    categoria: '',
+    descripcion: '',
+    imagen: ''
+  }
   constructor() { }
 
   ngOnInit() {
+   
+  }
+
+  guardarProduct(forma: NgForm) {
+    console.log('ngForm', forma);
+
   }
 
 }
