a5122708f54511ce8116f80a34a3e37619e83d6e
Florencia Cabrera <fcabrera@invico.gov.ar>
2020-01-29 12:12:55 -0300

    registro

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  usuario: UsuarioModel;
   constructor() { }
 
   ngOnInit() {
+    this.usuario = new UsuarioModel();
   }
 
-}
+  onSubmit(registro: NgForm) {
+    // if (registro.invalid) { return; }
+
+    console.log('Formulario enviado');
+    console.log(this.usuario);
+    console.log(registro);
+    }
+  }
+
+
---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,6 @@
+export class UsuarioModel {
+    name: string;
+    email: string;
+    password: string;
+
+}
