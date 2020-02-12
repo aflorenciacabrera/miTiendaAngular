ee166b7a4b294d85810576780f3065e3b464dfdb
Florencia Cabrera <fcabrera@invico.gov.ar>
2020-02-12 13:00:29 -0300

    registro anda

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from 'src/app/models/usuario.model';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import swal from 'bootstrap-sweetalert';
 
 @Component({
   selector: 'app-register',
@@ -9,8 +10,14 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  status: string;
   usuario: UsuarioModel;
+
+  usuarioLimpiar = {
+    name: '',
+    email: '',
+    password: '',
+  };
   constructor( private auth: AuthService) { }
 
   ngOnInit() {
@@ -18,11 +25,42 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(registro: NgForm) {
-     if (registro.invalid) { return; }
+    if (registro.invalid) { return; }
 
-     this.auth.nuevoUsuario(this.usuario).subscribe( resp => {
-       console.log(resp);
-     });
+    //  this.auth.nuevoUsuario(this.usuario).subscribe( resp => {
+    //    console.log(resp);
+    this.auth.register(this.usuario).subscribe(
+      resp => {
+        this.status = resp.status;
+        if (this.status == 'success') {
+          // Vaciar el formulario
+          //  this.usuario = this.usuarioLimpiar;
+          swal("Here's a message!")
+          swal("Good job!", "You clicked the button!", "success");
+          // swal('Exito!', 'El Usuario fue registrado Correctamente', 'success').then((resuta) => {
+          //   if (resuta.value) {
+          //     // Redirect
+          //     registro.reset();
+
+          //     // window.location.reload();
+
+          //   }
+          // });
+
+          //  console.log(resp.message);
+        } else {
+
+          this.status = 'error';
+          swal('Oops!', 'Este Usuario ya se encuentra registrado', 'error');
+          console.log(resp.error.message);
+        }
+      },
+      error =>{
+        // this.status = 'error';
+        swal('Oops!', 'Este Usuario ya se encuentra registrado', 'error');
+        // console.log(error.error.message);
+      });
+
     }
   }
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHandler, HttpHeaders} from '@angular/common/http';
 import { UsuarioModel } from '../models/usuario.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-    private url = 'http://127.0.0.1:8000/api';
+    public url: string;
 
   // crear nuevo usuario
 
   // login
 
 
-  constructor( private http: HttpClient) { }
+  constructor( public http: HttpClient) {
+    this.url = 'http://127.0.0.1:8000/api';
+  }
 
   logout() {
 
@@ -33,11 +36,32 @@ export class AuthService {
       // forma simplificada de la creación del objeto pero trae completo las propiedades
       ...usuario,
       getToken: true
-     
+
     };
     const url = this.url + '/register';
     return this.http.post(url, authData //  `${this.url}/register` info que se manda al servidor por pos
      );
+  }
+
+ 
+
+  register(usuario: UsuarioModel): Observable<any> {
+    const authData = {
+      // constuccion del objeto
+      // email: usuario.email,
+      // password: usuario.password,
+
+      // forma simplificada de la creación del objeto pero trae completo las propiedades
+      ...usuario,
+      getToken: true
+
+    };
+    const json = JSON.stringify(authData);
+    const params = 'json=' + json;
+    const url = this.url + '/register';
+     const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+    return this.http.post(url , params, {headers});
   }
 
 }
