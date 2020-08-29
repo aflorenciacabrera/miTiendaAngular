b40f9b0ca6cc474b0188f02cfa75ead38a4c0a11
ivan alejandro sambrana <=>
2020-08-29 16:03:16 -0300

    Ajusto las imagenes a la galeria para que no se vea estirada las fotos

--- src/app/components/productos/producto-galeria/producto-galeria.component.ts
+++ src/app/components/productos/producto-galeria/producto-galeria.component.ts
@@ -2,14 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ProductoService } from 'src/app/services/producto.service';
 import { ProductoModel } from 'src/app/models/producto.model';
 
+
 @Component({
   selector: 'app-producto-galeria',
   templateUrl: './producto-galeria.component.html',
   styleUrls: ['./producto-galeria.component.css']
 })
 export class ProductoGaleriaComponent implements OnInit {
-  producto : ProductoModel = new ProductoModel();
-  
+  // producto : ProductoModel = new ProductoModel();
+  producto:[];
   url ="http://127.0.0.1:8000/storage/";
  
   constructor(
